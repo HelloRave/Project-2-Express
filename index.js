85b27a10aea497488e5540e498641fb6e0b29e62
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-07-18 02:56:47 +0000

    Added and test get/post review routes on ARC

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const express = require('express');
 const axios = require('axios');
 const ObjectId = require('mongodb').ObjectId;
 const MongoUtil = require('./MongoUtil');
-const {checkEmptyFields, checkEmptyReviews} = require('./Middleware')
-const cors = require('cors'); 
+const { checkEmptyFields, checkEmptyReviews } = require('./Middleware')
+const cors = require('cors');
 require('dotenv').config()
 
 let app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors())
 
 const MONGO_URI = process.env.MONGO_URI
 
 // Routes
-async function main(){
+async function main() {
 
     const db = await MongoUtil.connect(MONGO_URI, 'manga_library')
 
-    app.get('/', async function(req, res){
+    app.get('/', async function (req, res) {
         const mangaRecords = await db.collection('manga_records').find({}).toArray()
         res.json(mangaRecords)
     })
 
     // Route to add new manga to the list - how to link with the other 3 collections? 
-    app.post('/add_new_manga', [checkEmptyFields, checkEmptyReviews], async function(req, res){
+    app.post('/add_new_manga', [checkEmptyFields, checkEmptyReviews], async function (req, res) {
 
         let author_id = ObjectId()
 
-        if (req.body.author_id){
+        if (req.body.author_id) {
             author_id = ObjectId(req.body.author_id)
         } else {
             await db.collection('manga_authors').insertOne({
@@ -38,7 +38,7 @@ async function main(){
 
         let review_id = ObjectId()
 
-        let url = req.body.url; 
+        let url = req.body.url;
         let title = req.body.title;
         let author = {
             _id: author_id,
@@ -61,7 +61,7 @@ async function main(){
         let mangaId = await db.collection('manga_records').findOne({
             'reviews': {
                 $in: [review_id]
-            } 
+            }
         }, {
             'projection': {
                 '_id': 1
@@ -86,12 +86,12 @@ async function main(){
         res.json(newManga) // can post to database but the error? 
     })
 
-    app.get('/find_author/:name', async function(req, res){
+    app.get('/find_author/:name', async function (req, res) {
         let results = await db.collection('manga_authors').find({
             author_name: {
                 $regex: req.params.name,
                 $options: 'i'
-                    }
+            }
         }).project({
             _id: 1
         }).toArray()
@@ -100,37 +100,73 @@ async function main(){
         res.send(results)
     })
 
-    app.get('/find_manga', async function(req, res){
+    app.get('/find_review/:review_id', async function (req, res) {
+        let reviewResults = await db.collection('manga_reviews').find({
+            _id: req.params.review_id
+        }).toArray()
+
+        res.json(reviewResults)
+    })
+
+    app.post('/add_review/:manga_id', async function (req, res) {
+        let manga = {
+            _id: ObjectId(req.params.manga_id),
+            name: req.body.title
+        }
+        let plot = req.body.plot;
+        let main_characters = req.body.main_characters;
+        let supporting_characters = req.body.supporting_characters;
+        let rating = req.body.rating
+
+        let addReview = await db.collection('manga_reviews').insertOne({
+            manga, plot, main_characters, supporting_characters, rating
+        })
+
+        // Insert this id into manga_records but doesnt work
+        let reviewToManga = await db.collection('manga_records').updateOne({
+            _id: ObjectId(req.params.manga_id)
+        }, {
+            '$push': {
+                'reviews': addReview.insertedId
+            }
+        })
+
+        console.log(addReview.insertedId, reviewToManga)
+
+        res.sendStatus(200)
+    })
+
+    app.get('/find_manga', async function (req, res) {
         let criteria = {}
 
-        if (req.query.author_name){
+        if (req.query.author_name) {
             criteria['author.name'] = {
                 $regex: req.query.author_name,
                 $options: 'i'
-                    }
-                }
-
-        if (req.query.title){
-            criteria['title'] = {
-                $regex: req.query.title,
-                $options: 'i'
-                    }
-                }
-
-        if (req.query.ongoing){
-            criteria['ongoing'] = {
-                $eq: req.query.ongoing == "true" ? true : false 
             }
         }
 
-        if (req.query.max_volume && req.query.min_volume){
+        if (req.query.title) {
+            criteria['title'] = {
+                $regex: req.query.title,
+                $options: 'i'
+            }
+        }
+
+        if (req.query.ongoing) {
+            criteria['ongoing'] = {
+                $eq: req.query.ongoing == "true" ? true : false
+            }
+        }
+
+        if (req.query.max_volume && req.query.min_volume) {
             criteria['volumes'] = {
                 $lte: Number(req.query.max_volume),
                 $gte: Number(req.query.min_volume)
             }
         }
 
-        if (req.query.max_chapter && req.query.min_chapter){
+        if (req.query.max_chapter && req.query.min_chapter) {
             criteria['chapters'] = {
                 $lte: Number(req.query.max_chapter),
                 $gte: Number(req.query.min_chapter)
@@ -145,11 +181,13 @@ async function main(){
         //     }
         // }
 
-        if (req.query.genre){
+        if (req.query.genre) {
             criteria['$and'] = req.query.genre.map((genre) => {
-                return {'genre': {
-                    '$in': [genre]
-                }}
+                return {
+                    'genre': {
+                        '$in': [genre]
+                    }
+                }
             })
         }
 
@@ -159,7 +197,7 @@ async function main(){
         res.send(results)
     })
 
-    app.patch('/update_manga/:id', checkEmptyFields, async function(req, res){
+    app.patch('/update_manga/:id', checkEmptyFields, async function (req, res) {
 
         let url = req.body.url
         let title = req.body.title;
@@ -168,7 +206,7 @@ async function main(){
             author_name: {
                 $regex: req.body.author_name,
                 $options: 'i'
-                    }
+            }
         }, {
             'projection': {
                 '_id': 1
@@ -176,9 +214,9 @@ async function main(){
         })
 
         let author = {
-            id_: (author_id && author_id._id) ? ObjectId(author_id._id) : ObjectId(), 
+            id_: (author_id && author_id._id) ? ObjectId(author_id._id) : ObjectId(),
             name: req.body.author_name
-        }; 
+        };
         let description = req.body.description;
         let genre = req.body.genre;
         let chapters = req.body.chapters;
@@ -204,7 +242,7 @@ main()
 
 // End of routes
 
-app.listen(8888, ()=>{
+app.listen(8888, () => {
     console.log('Server started')
 })
 
